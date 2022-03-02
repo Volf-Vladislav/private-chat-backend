596ff8cfcbe54f564622045a2f5505395d227680
Volf-Vladislav <43464334+Volf-Vladislav@users.noreply.github.com>
2022-03-02 06:28:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,63 +9,68 @@ const wss = new WebSocket.Server({
 }, () => console.log(`Server started on ${PORT}`))
 
 wss.on('connection', ws => {
-    ws.on('message', message => {
+    try {
+        ws.on('message', message => {
 
-        message = JSON.parse(message)
+            message = JSON.parse(message)
 
-        if (message.event == 'status') {
-            if (message.data.status == 'seeking') {
-                ws.gender = message.data.gender
-                ws.seekingGender = message.data.seekingGender
-                ws.id = message.data.id
+            if (message.event == 'status') {
+                if (message.data.status == 'seeking') {
+                    ws.gender = message.data.gender
+                    ws.seekingGender = message.data.seekingGender
+                    ws.id = message.data.id
 
-                wss.clients.forEach(client => {
-                    if (checkGender(client, ws)) {
-                        if (ws.status != 'chating' && client.status != 'chating') {
-                            ws.status = 'chating'
-                            client.status = 'chating'
-                            ws.seekingID = client.id
-                            client.seekingID = ws.id
+                    wss.clients.forEach(client => {
+                        if (checkGender(client, ws)) {
+                            if (ws.status != 'chating' && client.status != 'chating') {
+                                ws.status = 'chating'
+                                client.status = 'chating'
+                                ws.seekingID = client.id
+                                client.seekingID = ws.id
 
-                            if (ws.id != client.id) {
-                                ws.send(messageTemplate('finded', 'status', client.id))
-                                client.send(messageTemplate('finded', 'status', ws.id))
+                                if (ws.id != client.id) {
+                                    ws.send(messageTemplate('finded', 'status', client.id))
+                                    client.send(messageTemplate('finded', 'status', ws.id))
+                                }
                             }
                         }
-                    }
-                })
-            }
-
-            else if (message.data.status == 'print') {
-                wss.clients.forEach(client => {
-                    if (client.seekingID == ws.id) {
-                        client.send(messageTemplate('print', 'status'))
-                    }
-                })
-            }
-            else if (message.data.status == 'endPrint') {
-                wss.clients.forEach(client => {
-                    if (client.seekingID == ws.id) {
-                        client.send(messageTemplate('endPrint', 'status'))
-                    }
-                })
-            }
-        }
-
-        else if (message.event == 'message') {
-            wss.clients.forEach(client => {
-                if (client.seekingID == ws.id) {
-                    client.send(messageTemplate(message.data, 'message'))
+                    })
                 }
-            })
-        }
-    })
 
-    ws.on('close', () => {
+                else if (message.data.status == 'print') {
+                    wss.clients.forEach(client => {
+                        if (client.seekingID == ws.id) {
+                            client.send(messageTemplate('print', 'status'))
+                        }
+                    })
+                }
+                else if (message.data.status == 'endPrint') {
+                    wss.clients.forEach(client => {
+                        if (client.seekingID == ws.id) {
+                            client.send(messageTemplate('endPrint', 'status'))
+                        }
+                    })
+                }
+            }
+
+            else if (message.event == 'message') {
+                wss.clients.forEach(client => {
+                    if (client.seekingID == ws.id) {
+                        client.send(messageTemplate(message.data, 'message'))
+                    }
+                })
+            }
+        })
+
+        ws.on('close', () => {
             wss.clients.forEach(client => {
                 if (client.seekingID == ws.id && ws.id != client.id) {
                     client.send(messageTemplate('disconnected', 'status'))
                 }
             })
-    })
+        })
+    }
+    catch (err) {
+        console.log(err)
+    }
 })
